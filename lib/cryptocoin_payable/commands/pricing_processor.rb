065f970068267a0690bd2820bdedcfa7095017f3
Maros Hluska <mhluska@gmail.com>
2017-12-27 14:04:19 -0800

    Fix issue with rates not being defined before saving payment

--- lib/cryptocoin_payable/commands/pricing_processor.rb
+++ lib/cryptocoin_payable/commands/pricing_processor.rb
@@ -5,17 +5,23 @@ module CryptocoinPayable
     end
 
     def perform
+      rates = CurrencyConversion.coin_types.map { |coin_name|
+        [
+          coin_name[0].to_sym,
+          CurrencyConversion.create!(
+            # TODO: Store three previous price ranges, defaulting to 100 for now.
+            currency: 100,
+            price: Adapters.for(coin_name[0].to_sym).get_rate,
+          )
+        ]
+      }.to_h
+
       # Loop through all unpaid payments and update them with the new price if
       # it has been 30 mins since they have been updated.
       CoinPayment.where(state: [:pending, :partial_payment]).where("updated_at < ? OR coin_amount_due = 0", 30.minutes.ago).each do |payment|
-        # TODO: Store three previous price ranges, defaulting to 100 for now.
-        conversion = CurrencyConversion.create!(
-          currency: 100,
-          price: Adapters.for(payment.coin_type).get_rate
-        )
         payment.update!(
           coin_amount_due: payment.calculate_coin_amount_due,
-          coin_conversion: conversion.price
+          coin_conversion: rates[payment.coin_type].price,
         )
       end
     end

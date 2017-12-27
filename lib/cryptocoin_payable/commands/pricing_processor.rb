ee465a538abd3c0ea7f2307608634dfd54e998cc
Maros Hluska <mhluska@gmail.com>
2017-12-27 15:00:47 -0800

    Fix pricing processor issue related to to_sym

--- lib/cryptocoin_payable/commands/pricing_processor.rb
+++ lib/cryptocoin_payable/commands/pricing_processor.rb
@@ -5,23 +5,25 @@ module CryptocoinPayable
     end
 
     def perform
-      rates = CurrencyConversion.coin_types.map { |coin_name|
+      rates = CurrencyConversion.coin_types.map do |coin_pair|
+        coin_type = coin_pair[0].to_sym
         [
-          coin_name[0].to_sym,
+          coin_type,
           CurrencyConversion.create!(
             # TODO: Store three previous price ranges, defaulting to 100 for now.
             currency: 100,
-            price: Adapters.for(coin_name[0].to_sym).get_rate,
+            price: Adapters.for(coin_type).get_rate,
+            coin_type: coin_type,
           )
         ]
-      }.to_h
+      end.to_h
 
       # Loop through all unpaid payments and update them with the new price if
       # it has been 30 mins since they have been updated.
       CoinPayment.where(state: [:pending, :partial_payment]).where("updated_at < ? OR coin_amount_due = 0", 30.minutes.ago).each do |payment|
         payment.update!(
           coin_amount_due: payment.calculate_coin_amount_due,
-          coin_conversion: rates[payment.coin_type].price,
+          coin_conversion: rates[payment.coin_type.to_sym].price,
         )
       end
     end

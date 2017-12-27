b4ad3bf2e5c5319a5b514fa11859bbc899ebf8f1
Maros Hluska <mhluska@gmail.com>
2017-12-27 12:47:12 -0800

    Fix pricing processor reference errors

--- lib/cryptocoin_payable/commands/pricing_processor.rb
+++ lib/cryptocoin_payable/commands/pricing_processor.rb
@@ -7,7 +7,7 @@ module CryptocoinPayable
     def perform
       # Loop through all unpaid payments and update them with the new price if
       # it has been 30 mins since they have been updated.
-      CoinPayment.where(state: [:pending, :partial_payment]).where("updated_at < ? OR btc_amount_due = 0", 30.minutes.ago).each do |payment|
+      CoinPayment.where(state: [:pending, :partial_payment]).where("updated_at < ? OR coin_amount_due = 0", 30.minutes.ago).each do |payment|
         # TODO: Store three previous price ranges, defaulting to 100 for now.
         conversion = CurrencyConversion.create!(
           currency: 100,
@@ -15,7 +15,7 @@ module CryptocoinPayable
         )
         payment.update!(
           coin_amount_due: payment.calculate_coin_amount_due,
-          coin_conversion: rate.price
+          coin_conversion: conversion.price
         )
       end
     end

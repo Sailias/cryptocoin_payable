9317f54fc05468fca5967436429fd4ca4633d018
Maros Hluska <mhluska@gmail.com>
2018-03-19 19:32:12 -0700

    Move transaction sync to its own method

--- lib/cryptocoin_payable/commands/payment_processor.rb
+++ lib/cryptocoin_payable/commands/payment_processor.rb
@@ -4,6 +4,34 @@ module CryptocoinPayable
       new.perform
     end
 
+    def self.update_transactions_for(payment)
+      transactions = Adapters.for(payment.coin_type).get_transactions_for(payment.address)
+
+      transactions.each do |tx|
+        tx.symbolize_keys!
+
+        transaction = payment.transactions.find_by_transaction_hash(tx[:txHash])
+        if transaction
+          transaction.update(confirmations: tx[:confirmations])
+        else
+          payment.transactions.create!(
+            estimated_value: tx[:estimatedTxValue],
+            transaction_hash: tx[:txHash],
+            block_hash: tx[:blockHash],
+            block_time: tx[:blockTime],
+            estimated_time: tx[:estimatedTxTime],
+            coin_conversion: payment.coin_conversion,
+            confirmations: tx[:confirmations]
+          )
+
+          payment.update(
+            coin_amount_due: payment.calculate_coin_amount_due,
+            coin_conversion: CurrencyConversion.where(coin_type: payment.coin_type).last.price
+          )
+        end
+      end
+    end
+
     def perform
       CoinPayment.where(state: [:pending, :partial_payment, :paid_in_full]).each do |payment|
         # Check for completed payment first, in case it's 0 and we don't need to
@@ -13,34 +41,10 @@ module CryptocoinPayable
         next if payment.confirmed?
 
         begin
-          transactions = Adapters.for(payment.coin_type).get_transactions_for(payment.address)
+          self.class.update_transactions_for(payment)
         rescue JSON::ParserError
           STDERR.puts 'Error processing response from server. Possible API issue or your Quota has been exceeded'
           next
-        end
-
-        transactions.each do |tx|
-          tx.symbolize_keys!
-
-          transaction = payment.transactions.find_by_transaction_hash(tx[:txHash])
-          if transaction
-            transaction.update(confirmations: tx[:confirmations])
-          else
-            payment.transactions.create!(
-              estimated_value: tx[:estimatedTxValue],
-              transaction_hash: tx[:txHash],
-              block_hash: tx[:blockHash],
-              block_time: tx[:blockTime],
-              estimated_time: tx[:estimatedTxTime],
-              coin_conversion: payment.coin_conversion,
-              confirmations: tx[:confirmations]
-            )
-
-            payment.update(
-              coin_amount_due: payment.calculate_coin_amount_due,
-              coin_conversion: CurrencyConversion.where(coin_type: payment.coin_type).last.price
-            )
-          end
         end
 
         # Check for payments after the response comes back.

24f522e7f061507320ca693dc2c9a11009865871
Maros Hluska <mhluska@gmail.com>
2018-10-15 23:53:03 +0700

    Add fallback if bulk insert not supported

--- lib/cryptocoin_payable/commands/payment_processor.rb
+++ lib/cryptocoin_payable/commands/payment_processor.rb
@@ -7,7 +7,47 @@ module CryptocoinPayable
     end
 
     def self.update_transactions_for(payment)
+      new.update_transactions_for(payment)
+    end
+
+    def perform
+      CoinPayment.unconfirmed.find_each do |payment|
+        # Check for completed payment first, in case it's 0 and we don't need to
+        # make an API call.
+        update_payment_state(payment)
+
+        next if payment.confirmed?
+
+        begin
+          update_transactions_for(payment)
+        rescue StandardError => error
+          STDERR.puts 'PaymentProcessor: Unknown error encountered, skipping transaction'
+          STDERR.puts error
+          next
+        end
+
+        # Check for payments after the response comes back.
+        update_payment_state(payment)
+
+        # If the payment has not moved out of the pending state after loading
+        # new transactions, we expire it.
+        update_payment_expired_state(payment) if payment.pending?
+      end
+    end
+
+    def update_transactions_for(payment)
       transactions = Adapters.for(payment.coin_type).fetch_transactions(payment.address)
+
+      if ActiveRecord::Base.connection.supports_on_duplicate_key_update?
+        update_via_bulk_insert(payment, transactions)
+      else
+        update_via_many_insert(payment, transactions)
+      end
+    end
+
+    private
+
+    def update_via_bulk_insert(payment, transactions)
       transactions.each do |t|
         t[:coin_conversion] = payment.coin_conversion
         t[:coin_payment_id] = payment.id
@@ -26,32 +66,18 @@ module CryptocoinPayable
       end
     end
 
-    def perform
-      CoinPayment.unconfirmed.find_each do |payment|
-        # Check for completed payment first, in case it's 0 and we don't need to
-        # make an API call.
-        update_payment_state(payment)
-
-        next if payment.confirmed?
-
-        begin
-          self.class.update_transactions_for(payment)
-        rescue StandardError => error
-          STDERR.puts 'PaymentProcessor: Unknown error encountered, skipping transaction'
-          STDERR.puts error
-          next
+    def update_via_many_insert(payment, transactions)
+      transactions.each do |tx|
+        transaction = payment.transactions.find_by_transaction_hash(tx[:tx_hash])
+        if transaction
+          transaction.update(confirmations: tx[:confirmations])
+        else
+          tx[:coin_conversion] = payment.coin_conversion
+          payment.transactions.create!(tx)
+          payment.update_coin_amount_due
         end
-
-        # Check for payments after the response comes back.
-        update_payment_state(payment)
-
-        # If the payment has not moved out of the pending state after loading
-        # new transactions, we expire it.
-        update_payment_expired_state(payment) if payment.pending?
       end
     end
-
-    protected
 
     def update_payment_state(payment)
       if payment.currency_amount_paid >= payment.price
